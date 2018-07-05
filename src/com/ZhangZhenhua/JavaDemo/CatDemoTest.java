547ca0b3706a3fd2913c9c6950c70c54e7d85731
resticDOG <1096340650@qq.com>
2018-07-05 11:31:44 +0800

    更新

--- src/com/ZhangZhenhua/JavaDemo/CatDemoTest.java
+++ src/com/ZhangZhenhua/JavaDemo/CatDemoTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CatDemoTest {
 	public static void main(String[] args) {
 		
-		CatDemo cat1 = new CatDemo("小公主", 0.3f, "女", "英国短毛猫", "蓝色");
+		CatDemo cat1 = new CatDemo("", 0.3f, "女", "英国短毛猫", "蓝色");
 		System.out.println("请给小猫取个名吧（注意：取了名字之后无法更改）:");
 		String firstName = new Scanner(System.in).next();
 		cat1 = new CatDemo(firstName);
